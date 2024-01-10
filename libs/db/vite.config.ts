6fec8ff3bd322e3807b337245bf6c06c484c780f
Jace Babin <jace.babin@kcftech.com>
2024-01-10 12:06:19 -0700

    add basic lib projects for api, db, and ui
    Signed-off-by: Jace Babin <jace.babin@kcftech.com>

---
+++ libs/db/vite.config.ts
@@ -0,0 +1,44 @@
+/// <reference types='vitest' />
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es', 'cjs'],
+
+      name: 'db',
+    },
+    outDir: '../../dist/libs/db',
+    reportCompressedSize: true,
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: [],
+    },
+  },
+  cacheDir: '../../node_modules/.vite/libs/db',
+  plugins: [
+    nxViteTsPaths(),
+    dts({
+      entryRoot: 'src',
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+    }),
+  ],
+  root: __dirname,
+});
