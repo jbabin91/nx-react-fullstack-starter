56d969a06d5df8594449391af3d1ac6c39884276
Jace Babin <jbabin91@gmail.com>
2024-01-18 14:28:37 -0700

    init project
    Signed-off-by: Jace Babin <jace.babin@kcftech.com>

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,49 +1,43 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/frontend',
-
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
   build: {
-    outDir: '../../dist/apps/frontend',
-    reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
     },
+    outDir: '../../dist/apps/frontend',
+    reportCompressedSize: true,
   },
-
+  cacheDir: '../../node_modules/.vite/apps/frontend',
+  plugins: [react(), nxViteTsPaths()],
+  preview: {
+    host: 'localhost',
+    port: 4300,
+  },
+  root: __dirname,
+  server: {
+    host: 'localhost',
+    port: 4200,
+  },
   test: {
-    globals: true,
     cache: {
       dir: '../../node_modules/.vitest',
     },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-    reporters: ['default'],
     coverage: {
-      reportsDirectory: '../../coverage/apps/frontend',
       provider: 'v8',
+      reportsDirectory: '../../coverage/apps/frontend',
     },
+    environment: 'jsdom',
+    globals: true,
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
   },
 });
