80b5d3f949e6e263739ecb70b2a0bbdae2126feb
Jace Babin <jbabin91@gmail.com>
2024-01-18 13:00:33 -0700

    add api functions for user and post
    Signed-off-by: Jace Babin <jace.babin@kcftech.com>

--- libs/api/src/index.ts
+++ libs/api/src/index.ts
@@ -1,2 +1,4 @@
 export { type Context, createContext } from './lib/context';
 export { type AppRouter, appRouter } from './lib/routers';
+export * from './schemas/posts';
+export * from './schemas/users';
--- libs/api/src/lib/routers/post/post.db.ts
+++ libs/api/src/lib/routers/post/post.db.ts
@@ -1,19 +1,110 @@
 import { PrismaClient } from '@prisma/client';
 
+import {
+  type CreatePost,
+  createPostSchema,
+  type Post,
+  type Posts,
+  type UpdatePost,
+  updatePostSchema,
+} from '../../../schemas/posts';
+
 const prisma = new PrismaClient();
 
-export const getPosts = async () => await prisma.post.findMany();
+/**
+ * Retrieves all posts from the database.
+ * @returns {Promise<Posts>} A promise that resolves to an array of posts.
+ */
+export const getPosts = async (): Promise<Posts> => {
+  const posts = await prisma.post.findMany();
 
-export const getPostById = async (id: string) =>
-  await prisma.post.findUnique({
+  return posts;
+};
+
+/**
+ * Retrieves a post by its ID.
+ * @param id - The ID of the post.
+ * @returns {Promise<Post | null>} A promise that resolves to the post object if found, or null if not found.
+ */
+export const getPostById = async (id: string): Promise<Post | null> => {
+  const post = await prisma.post.findUnique({
     where: {
       id,
     },
   });
 
-export const getPostsByUserId = async (id: string) =>
-  await prisma.post.findMany({
+  return post;
+};
+
+/**
+ * Retrieves posts by user ID.
+ * @param id - The ID of the user.
+ * @returns {Promise<Posts>} A promise that resolves to an array of posts.
+ */
+export const getPostsByUserId = async (id: string): Promise<Posts> => {
+  const posts = await prisma.post.findMany({
     where: {
       authorId: id,
     },
   });
+
+  return posts;
+};
+
+/**
+ * Creates a new post.
+ * @param {CreatePost} data The data for the post.
+ * @returns {Promise<Post>} A promise that resolves to the created post.
+ * @throws {Error} An error if the post data is invalid.
+ */
+export const createPost = async (data: CreatePost): Promise<Post> => {
+  const post = createPostSchema.safeParse(data);
+
+  if (!post.success) {
+    throw new Error('Invalid post data');
+  }
+
+  const createdPost = await prisma.post.create({
+    data: post.data,
+  });
+
+  return createdPost;
+};
+
+/**
+ * Updates a post in the database.
+ * @param {UpdatePost} data - The data to update the post with.
+ * @returns {Promise<Post>} A promise that resolves to the updated post.
+ * @throws {Error} An error if the post data is invalid.
+ */
+export const updatePost = async (data: UpdatePost): Promise<Post> => {
+  const post = updatePostSchema.safeParse(data);
+
+  if (!post.success) {
+    throw new Error('Invalid post data');
+  }
+
+  const updatedPost = await prisma.post.update({
+    data: post.data,
+    where: {
+      id: post.data.id,
+    },
+  });
+
+  return updatedPost;
+};
+
+/**
+ * Deletes a post from the database.
+ * @param {string} id - The ID of the post to delete.
+ * @returns {Promise<Post>} A Promise that resolves to the deleted post.
+ */
+export const deletePost = async (id: string): Promise<Post> => {
+  const deletedPost = await prisma.post.delete({
+    where: {
+      id,
+    },
+  });
+
+  return deletedPost;
+};
